2d48f7d709fe87871a741d588ca9062d656395f3
Quoc-Hwng <phamhung151020@gmail.com>
2024-04-18 15:26:00 +0700

    units

--- db/queries.ts
+++ db/queries.ts
@@ -1,4 +1,4 @@
-import { courses } from '@/db/schema';
+import { courses, units } from '@/db/schema';
 import db from "@/db/drizzle";
 import { auth } from "@clerk/nextjs";
 import { cache } from "react";
@@ -18,6 +18,43 @@ export const getUserProgress = cache(async () => {
         },
     });
     return data;
+})
+
+export const getUnits = cache(async () => {
+    const userProgress = await getUserProgress();
+
+    if(!userProgress?.activeCourseId) {
+        return [];
+    }
+
+    const data = await db.query.units.findMany({
+        where: eq(units.courseId, userProgress.activeCourseId),
+        with: {
+            lessons: {
+                with: {
+                    challenges: {
+                        with: {
+                            challengeProgress: true
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    const normalizedData = data.map((unit) => {
+        const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+            const allCompletedChallenges = lesson.challenges.every((challenge) => {
+                return challenge.challengeProgress
+                    && challenge.challengeProgress.length > 0
+                    && challenge.challengeProgress.every((progress) => progress.completed);
+            });
+
+            return { ...lesson, completed: allCompletedChallenges}
+        })
+        return { ...unit, lessons: lessonsWithCompletedStatus }
+    });
+    return normalizedData
 })
 
 export const getCourses = cache(async () => {
